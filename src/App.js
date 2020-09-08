666959ec1e671970b7092e290bd9db1168b047fe
waltinhocalegari <waltinhocalegari@hotmail.com>
2020-09-08 20:34:52 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Comercio</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
